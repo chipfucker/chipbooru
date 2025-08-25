1707fc4c07d5cfbc08d99d0490305752076ee299
betterboruu <nightofthebuttlickers@gmail.com>
2025-08-25 16:20:37 -0500

    generic save

--- b/r34/post/main.js
+++ b/r34/post/main.js
@@ -1,5 +1,7 @@
-con.log("butt");
-con.warn("butt");
-con.error("butt");
-con.debug("butt");
-con.info("butt");
+window.addEventListener("load", () => {
+    con.log("butt");
+    con.warn("butt");
+    con.error("butt");
+    con.debug("butt");
+    con.info("butt");
+});
