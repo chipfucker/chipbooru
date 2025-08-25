20f76ee0fe94c2030a9d960d1438ba31fafa89f4
chiptumor <141371172+chiptumor@users.noreply.github.com>
2025-08-25 12:39:37 -0500

    enable console subscriber

--- b/r34/post/main.js
+++ b/r34/post/main.js
@@ -1,6 +1,13 @@
 const debug = true;
-
-const element = {};
+/*** CONSOLE SUBSCRIER ***/
+(function() {
+	let callback = (category, args) => {
+		let message = `<div class="${category}">${
+			args.JSON.stringify(args, null, "\t") }</div>`;
+		document.getElementById("console").innerHTML += message; 
+	};
+	cs.bind(callback);
+})();
 
 /*** LUCIDE SVG ICONS ***/
 window.addEventListener("load", function() {
