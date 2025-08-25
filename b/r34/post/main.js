ebd561f6a0eb790eea07e095977de8ede66d088c
chiptumor <141371172+chiptumor@users.noreply.github.com>
2025-08-25 12:47:06 -0500

    change console subscriber to event listener

--- b/r34/post/main.js
+++ b/r34/post/main.js
@@ -1,13 +1,13 @@
 const debug = true;
 /*** CONSOLE SUBSCRIER ***/
-(function() {
+window.addEventListener("load", function() {
 	let callback = (category, args) => {
 		let message = `<div class="${category}">${
 			args.JSON.stringify(args, null, "\t") }</div>`;
 		document.getElementById("console").innerHTML += message; 
 	};
 	cs.bind(callback);
-})();
+});
 
 /*** LUCIDE SVG ICONS ***/
 window.addEventListener("load", function() {
