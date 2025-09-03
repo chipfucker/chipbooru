76734d32769fd95e5e10ff0b9757caa3ed5c1edf
chipfucker <lizzpegschris@gmail.com>
2025-09-02 21:05:13 -0500

    api url

--- b/r34/post/main.js
+++ b/r34/post/main.js
@@ -1,5 +1,7 @@
 // &api_key=e3541e79620b91846e0054b863b4f03dad3904bdce414cf99d7cd70df57cd48698cc42b5409aee0459685640157c9d4bdfbc32582c292953375b1ccc6ee1e3ef&user_id=2373207
 
+// https://api.rule34.xxx//index.php?page=dapi&s=post&q=index${jsonBool?"&json=1&fields=tag_info":""}&id=`
+
 const debug = true;
 
 window.addEventListener("load", () => {
