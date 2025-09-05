1d1b41d6dae7103e9ae9dd1b76da3941eb5d6d55
chiptumor <141371172+chiptumor@users.noreply.github.com>
2025-09-05 16:06:00 +0000

    set up server capabilities

---
+++ server.js
@@ -0,0 +1,14 @@
+const base = "chipbooru";
+
+const express = require("express");
+const app = express();
+const PORT = 5500;
+
+app.use("/"+base, express.static(__dirname));
+app.get("/", (req, res) => {
+	res.redirect(`/${base}`);
+});
+
+app.listen(PORT, () => {
+	console.log(`Server running at http://127.0.0.1:${PORT}/${base}/`);
+});
